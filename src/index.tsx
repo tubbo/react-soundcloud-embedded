c438c063f2d460cdf3a6374ce86a70f858d6924a
h0tw4t3r <h0tw4t3r@parrotsec.org>
2020-06-07 11:32:44 +0300

    typings fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,30 @@
 import React, { ReactElement } from 'react'
 
 export type SoundCloudEmbeddedProps = {
-  width: number | string
-  height: number | string
+  width?: number | string
+  height?: number | string
   url: string
-  autoPlay: boolean
-  hideRelated: boolean
-  showComments: boolean
-  showUser: boolean
-  showReposts: boolean
-  visual: boolean
-  color: string
+  autoPlay?: boolean
+  hideRelated?: boolean
+  showComments?: boolean
+  showUser?: boolean
+  showReposts?: boolean
+  visual?: boolean
+  color?: string
 }
 
-export default function SoundCloudEmbedded(
-  {
-    url,
-    width,
-    height,
-    autoPlay,
-    hideRelated,
-    showComments,
-    showUser,
-    showReposts,
-    visual,
-    color
-  }: SoundCloudEmbeddedProps = {
-    width: '100%',
-    height: '450px',
-    url: 'https://soundcloud.com/icebound/dusty-breaks-at-the-bottom-of-the-random-crates',
-    autoPlay: false,
-    hideRelated: false,
-    showComments: true,
-    showUser: true,
-    showReposts: false,
-    visual: true,
-    color: 'ff5500'
-  }
-): ReactElement {
+export default function SoundCloudEmbedded({
+  url,
+  width = '100%',
+  height = '450px',
+  autoPlay = false,
+  hideRelated = false,
+  showComments = false,
+  showUser = true,
+  showReposts = false,
+  visual = true,
+  color = 'ff5500'
+}: SoundCloudEmbeddedProps): ReactElement {
   const src = visual
     ? `https://w.soundcloud.com/player/?url=${url}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}&amp;visual=${visual}`
     : `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}`
